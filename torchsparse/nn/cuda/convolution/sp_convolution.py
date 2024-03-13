bb30b6ab02f0cc2244d9940d3432549dcf0db235
nightelf <guolicheng800@163.com>
2024-03-13 16:02:53 +0800

    修改conv测试文件

--- torchsparse/nn/cuda/convolution/sp_convolution.py
+++ torchsparse/nn/cuda/convolution/sp_convolution.py
@@ -10,21 +10,21 @@ class SPConvolution(Cell):
     def __init__(self, ):
         super(SPConvolution, self).__init__()
 
-        def infer_func(a, b, c, d, e):
+        def infer_func(a, b, c, d, e, f):
             return b
 
-        spvoxelize_back = ops.Custom("./voxelize_cuda.cu:voxelize_backward_ms",
-                            infer_func,
-                            infer_func,
-                            func_type="aot")
-        def bprop(top_grad, idx, counts, N):
-            return spvoxelize_back(top_grad, idx, counts, N)
-
-        self.spvoxelize = ops.Custom("./voxelize_cuda.cu:voxelize_forward_ms",
-                            infer_func,
-                            infer_func,
-                            func_type="aot",
-                            bprop=bprop)
+        # spvoxelize_back = ops.Custom("./voxelize_cuda.cu:voxelize_backward_ms",
+        #                     infer_func,
+        #                     infer_func,
+        #                     func_type="aot")
+        # def bprop(top_grad, idx, counts, N):
+        #     return spvoxelize_back(top_grad, idx, counts, N)
+        #
+        # self.spvoxelize = ops.Custom("./voxelize_cuda.cu:voxelize_forward_ms",
+        #                     infer_func,
+        #                     infer_func,
+        #                     func_type="aot",
+        #                     bprop=bprop)
         self.spconvolution = ops.Custom("./convolution_cuda.so:convolution_forward_ms",
                                      out_shape=infer_func,
                                      out_dtype=infer_func,
@@ -32,13 +32,13 @@ class SPConvolution(Cell):
 
     def construct(self, in_feat, out_feat, kernel, neighbor_map, neighbor_offset, transpose):
         print("---cuda_begin---")
-        return self.spconvolution(in_feat, out_feat, kernel, neighbor_map, neighbor_offset)
+        return self.spconvolution(in_feat, out_feat, kernel, neighbor_map, neighbor_offset, transpose)
 
 
 if __name__ == '__main__':
     context.set_context(device_target='GPU')
 
-    sample = np.load("/home/ubuntu/hdd1/ylc/codes/torchsparse-1.4.0/examples/conv_sample.npz")
+    sample = np.load("conv_forward_sample.npz")
     
     print("input.type: ", sample["input"].dtype)
     print("output.type: ", sample["output"].dtype)
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     weight = ms.Tensor(sample["weight"], dtype=ms.float32)
     nbmaps = ms.Tensor(sample["nbmaps"], dtype=ms.int32)
     nbsizes = ms.Tensor(sample["nbsizes"], dtype=ms.int32)
-    transposed = False
+    transposed = sample["transposed"].item()
 
     print(f"input.shape:{input.shape}")
     print(f"output.shape:{output.shape}")
